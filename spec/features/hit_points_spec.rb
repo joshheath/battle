381e63e791182c3026fb7556c7f4d4c102698167
Josh Heath <37807868+joshheath@users.noreply.github.com>
2018-05-30 16:37:17 +0100

    added web_helpers to refactor and tidy up tests

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,9 +1,6 @@
 feature 'View hit points' do
   scenario 'see Player 2 hit points' do
-    visit('/')
-    fill_in :player_1_name, with: 'Josh'
-    fill_in :player_2_name, with: 'Solomon'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Solomon: 60HP'
   end
 end
