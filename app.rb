5da453c6c6e13843d2212ff88c47bd5e30849661
Josh Heath <37807868+joshheath@users.noreply.github.com>
2018-06-01 14:44:21 +0100

    added player class

--- app.rb
+++ app.rb
@@ -1,30 +1,28 @@
 require 'sinatra/base'
+require 'player'
 
 class Battle < Sinatra::Base
   enable :sessions
 
   get '/' do
-    # 'Testing infrastructure working!'
     erb :index
   end
 
   post '/names' do
-    session[:player_1_name] = params[:player_1_name]
-    session[:player_2_name] = params[:player_2_name]
-    # @player_1_name = params[:player_1_name]
-    # @player_2_name = params[:player_2_name]
+    $player_1 = Player.new(params[:player_1_name])
+    $player_2 = Player.new(params[:player_2_name])
     redirect '/play'
   end
 
   get '/play' do
-    @player_1_name = session[:player_1_name]
-    @player_2_name = session[:player_2_name]
+    @player_1 = $player_1.name
+    @player_2 = $player_2.name
     erb :play
   end
 
   get '/attack' do
-    @player_1_name = session[:player_1_name]
-    @player_2_name = session[:player_2_name]
+    @player_1 = $player_1
+    @player_2 = $player_2
     erb :attack
   end
 
