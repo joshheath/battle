fedbfa0aa21c88e413ed8b8f5f6d6e79c285f3a9
Josh Heath <37807868+joshheath@users.noreply.github.com>
2018-06-01 16:18:22 +0100

    added hit points tests and passed them

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -5,3 +5,12 @@ feature 'attack' do
     expect(page).to have_content 'Josh attacked Solomon'
   end
 end
+
+feature 'attacking' do
+  scenario 'reduce player 2 HP by 10' do
+    sign_in_and_play
+    click_link 'Attack'
+    expect(page).not_to have_content 'Solomon: 60HP'
+    expect(page).to have_content 'Solomon: 50HP'
+  end
+end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,9 +1,31 @@
 require 'player'
 
 describe Player do
-  subject(:josh) { Player.new("Josh")}
+  subject(:josh) { Player.new("Josh") }
+  subject(:solomon) { Player.new("Solomon") }
 
-  it 'returns the name' do
-    expect(josh.name).to eq 'Josh'
+  describe '#name' do
+    it 'returns the name' do
+      expect(josh.name).to eq 'Josh'
+    end
+  end
+
+  describe '#hit_points' do
+    it 'returns the hit points' do
+      expect(josh.hit_points).to eq described_class::DEFAULT_HIT_POINTS
+    end
+  end
+
+  describe '#attack' do
+    it 'damages the player' do
+      expect(solomon).to receive(:receive_damage)
+      josh.attack(solomon)
+    end
+  end
+
+  describe '#receive_damage' do
+    it 'reduces the player hit points' do
+      expect { josh.receive_damage }.to change { josh.hit_points }.by(-10)
+    end
   end
 end
